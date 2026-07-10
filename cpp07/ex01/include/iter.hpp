f348088b0b72ea497d6047547071c62668024ee3
zelie-delhotal <zelie.delhotal@proton.me>
2026-07-10 23:26:43 +0200

    start of ex01

---
+++ cpp07/ex01/include/iter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelhota@student.42perpignan.fr            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/10 23:00:08 by gdelhota          #+#    #+#             */
+/*   Updated: 2026/07/10 23:26:16 by gdelhota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template<typename T>
+void iter(T[] array, const int size, void f(T&))
+{
+	for (int i=0; i<size; i++)
+	{
+		f(array[i]);
+	}
+}
+
+#endif
