e2df2c6ebd93cd79b24458aada558d71e721211b
zelie.delhotal <zelie.delhotal@proton.me>
2026-07-21 10:34:30 +0200

    CPP09 file structure

---
+++ cpp09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdelhota@student.42perpignan.fr            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/19 14:32:05 by gdelhota          #+#    #+#             */
+/*   Updated: 2026/07/19 14:32:53 by gdelhota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+class BitcoinExchange {
+};
+
+#endif
